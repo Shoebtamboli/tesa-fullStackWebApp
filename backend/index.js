38a195156475cb109aca7f3bdb03cd68af71d58f
Shoeb Tamboli <shoeb.tamboli@tesa.com>
2022-01-11 14:53:09 +0100

    added vercel json

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger.json');
 
-const campaignRouter = require('./routes/campaign');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
+
+const campaignRouter = require("./routes/campaign");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: __dirname + "/.env" });
+}
+
+const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json()); //req.body
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 //ROUTES
 
-app.use('/api', campaignRouter);
+app.use("/api", campaignRouter);
+
+// static files (build of your frontend)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 // listen
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import CreateCampaign from './components/CreateCampaign';
-import ListCampaign from './components/ListCampaign';
-import { Nav, Navbar } from 'react-bootstrap';
-import EditCampaign from './components/EditCampaign';
-import logo from './images/01.png';
+import CreateCampaign from "./components/CreateCampaign";
+import ListCampaign from "./components/ListCampaign";
+import { Nav, Navbar } from "react-bootstrap";
+import EditCampaign from "./components/EditCampaign";
+import logo from "./images/01.png";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<div className="container">
-					<Navbar bg="light" expand="lg">
-						<Navbar.Brand href="/">
-							<img src={logo} width="130" height="50" alt="Tesa brand logo" />
-						</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="mr-auto">
-								<Nav.Link href="/create">Create Campaign</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
+  return (
+    <div className="App">
+      <Router basename="/">
+        <div className="container">
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">
+              <img src={logo} width="130" height="50" alt="Tesa brand logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/create">Create Campaign</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
 
-					<h1 className="header">KAMPAGNEN</h1>
+          <h1 className="header">KAMPAGNEN</h1>
 
-					<Route path="/" exact component={ListCampaign} />
-					<Route path="/edit/:id" exact component={EditCampaign} />
-					<Route path="/create" exact component={CreateCampaign} />
-				</div>
-			</Router>
-		</div>
-	);
+          <Route path="/" exact component={ListCampaign} />
+          <Route path="/edit/:id" exact component={EditCampaign} />
+          <Route path="/create" exact component={CreateCampaign} />
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
